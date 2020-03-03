097c8bf03ebfdf80ede8a22ab11c9081b76860d0
Gerald Whitehead <geraldw@Geralds-MacBook-Air.local>
2020-03-03 18:47:08 -0500

    new routes added

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const cors = require('cors');
 const bcrypt = require('bcrypt');
 const Sequelize = require('sequelize')
 const router = express.Router();
+const bodyParser = require('body-parser');
 
 const ProductsModel = require('./models/products');
 const UsersModel = require('./models/users');
@@ -44,18 +45,89 @@ const OrderProduct = OrderProductsModel(sequelize, Sequelize);
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express());
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/api/products', function (req, res) {
+    
+    Products.findAll().then((results) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(results));
+    }).catch(function (e) {
+        console.log(e);
+        res.status(434).send('error retrieving products');
+    })
+});
+
+app.get('/api/products/:id', function (req, res) {
+
+    let id = req.params.id;
+    
+    Products.findOne({ where: {id: id} }).then((results) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(results));
+    }).catch(function (e) {
+        console.log(e);
+        res.status(434).send('error retrieving product info');
+    })
+});
+
+app.get('/api/users', function (req, res) {
+    
+    Users.findAll().then((results) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(results));
+    }).catch(function (e) {
+        console.log(e);
+        res.status(434).send('error retrieving users');
+    })
+});
+
+app.get('/api/users/:id', function (req, res) {
+
+    let id = req.params.id;
+    
+    Users.findOne({ where: {id: id} }).then((results) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(results));
+    }).catch(function (e) {
+        console.log(e);
+        res.status(434).send('error retrieving user info');
+    })
+});
+
+app.post('/api/products', function (req, res) {
+    
+    let data = {
+        name: req.body.name,
+        category: req.body.category,
+        price: req.body.price,
+        description: req.body.description,
+        available_quantity: req.body.available_quantity,
+        image: req.body.image
+    };
+    
+    Products.create(data).then(function (product) {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(product));
+    }).catch(function (e) {
+        res.status(434).send('unable to list product')
+    })
+
+});
 
 
 
 
-// router.get('/register', function(req, res) {
+
     app.post('/api/register', function (req, res) {
+        console.log(req.body)
     
         let data = {
-            name: req.body.name.trim(),
-            email: req.body.email.toLowerCase().trim(),
+            name: req.body.name,
+            email: req.body.email,
             password: req.body.password,
             is_admin: req.body.is_admin,
             address_line1: req.body.address_line1,
@@ -64,29 +136,60 @@ app.use(cors());
             state: req.body.state,
             zip: req.body.zip
         };
-        
+        console.log(data)
         if (data.name && data.email && data.password && data.address_line1 && data.city && data.state && data.zip) {
-            
+            console.log(data)
             var salt = bcrypt.genSaltSync(10);
             var hash = bcrypt.hashSync(data.password, salt);
-            data['password_hash'] = hash;
+            data['password'] = hash;
+            
     
             Users.create(data).then(function (user) {
+                console.log(user)
                 res.setHeader('Content-Type', 'application/json');
                 res.end(JSON.stringify(user));
             });
-            // res.redirect('/login')
+    
         } else {
             
             res.status(434).send('Name, email, password, and address is required to register')
         }
-        res.redirect('/register')
+        
     });
-// })
 
 
 
 
+app.post('/api/login', function (req, res) {
+    
+    let email = req.body.email;
+    let password = req.body.password;
+    console.log(email)
+    if (email && password) {
+        Users.findOne({
+            where: {
+                email: email
+            },
+        }).then((results) => {
+            console.log(results)
+            bcrypt.compare(password, results.password).then(function (matched) {
+                if (matched) {
+                    // req.session.user = results.id;
+                    // req.session.name = results.name;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.end(JSON.stringify(results));
+                } else {
+                    res.status(434).send('Email/Password combination did not match')
+                }
+            });
+        }).catch((e) => {
+            res.status(434).send('Email does not exist in the database')
+        });
+    } else {
+        res.status(434).send('Both email and password is required to login')
+    }
+    
+});
 
 
 
